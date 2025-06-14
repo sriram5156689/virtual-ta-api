320e414462b9f0d284d9af88117e795fc6233456
sriram5156689 <151396211+sriram5156689@users.noreply.github.com>
2025-06-14 21:04:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -725,4 +725,4 @@ async def health_check():
         )
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True) 
+    uvicorn.run("app:app", host="0.0.0.0", port=10000, reload=True) 
